2dfbf317814dd8d36c4013869eda7e01df8e6f43
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-06-13 11:32:01 +0700

    Variable and Datatype, Convert String

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,40 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+    /*
+        Variable & Data type
+    */
+////        Integer
+//        int height = 170;
+//        System.out.println(height);
+////        Double
+//        double grade = 1.70;
+//        System.out.println(grade);
+////        String
+//        String name = "Pramote";
+//        System.out.println(name);
+////        Boolean
+//        boolean isPass = true;
+//        System.out.println(isPass);
+
+    /*
+        Concat variables
+    */
+//        double lat = 13.5555;
+//        double lon = 100.5555;
+//        String outputText = "("+ lat + "," + lon + ")";
+//        System.out.println(outputText);
+
+    /*
+            Convert String
+    */
+//        To integer
+        String numberText = "23";
+        int number = Integer.parseInt(numberText);
+        System.out.println(number + 5);
+//        To double
+        String textDouble = "23.77";
+        double numberDouble = Double.parseDouble(textDouble);
+        System.out.println(numberDouble + 0.2);
     }
 }
